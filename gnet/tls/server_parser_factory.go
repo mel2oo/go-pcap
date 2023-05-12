ea513c2dd843c66ae4824cbd93bea47471401b58
ac0d3r <admin@imipy.com>
2023-05-12 14:44:16 +0800

    Update other

--- gnet/tls/server_parser_factory.go
+++ gnet/tls/server_parser_factory.go
@@ -32,7 +32,7 @@ func (factory *tlsServerParserFactory) Accepts(input memview.MemView, isEnd bool
 var serverHelloHandshakeBytes = []byte{
 	// Record header (5 bytes)
 	0x16,       // handshake record
-	0x03, 0x00, // protocol version 3.3 (TLS 1.2)
+	0x03, 0x00, // protocol version 3.x (TLS 1.2)
 	0x00, 0x00, // handshake payload size (ignored)
 
 	// Handshake header (4 bytes)
@@ -40,7 +40,7 @@ var serverHelloHandshakeBytes = []byte{
 	0x00, 0x00, 0x00, // Server Hello payload size (ignored)
 
 	// Server Version (2 bytes)
-	0x03, 0x00, // protocol version 3.3 (TLS 1.2)
+	0x03, 0x00, // protocol version 3.x (TLS 1.0)
 }
 
 var serverHelloHandshakeMask = []byte{
